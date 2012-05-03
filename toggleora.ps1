0f4161db7c83f39ca2c0a87aee9e2e166c5d39b9
Dan Ward <dan.j.ward@gmail.com>
2012-05-03 13:23:07 +0100

    updates for work machine

---
+++ toggleora.ps1
@@ -0,0 +1,31 @@
+﻿# PowerShell cmdlet to check a service's status
+$services = "OracleServiceORCL", "OracleOraDb10g_home1TNSListener", "OracleOraDb10g_home1iSQL*Plus", "OracleDBConsoleorcl"
+$servicePrior = get-Service $services[0]
+$services[0] + " is currently " + $servicePrior.status
+$turnOff = $servicePrior.status -eq  "Running"
+
+foreach ($serviceName in $services)
+{
+	$actualStatus = ((get-Service $serviceName).status -eq "Running")
+	if ($turnOff)
+	{
+		if ($actualStatus)
+		{
+			"Stopping " + $serviceName
+			stop-service $serviceName -Force
+			$action = "stopped"
+		}
+	}
+	else
+	{
+		if (!$actualStatus)
+		{
+			"Starting " + $serviceName
+			start-service $serviceName
+			$action = "started"
+		}
+	}
+}
+
+
+"The server is now: " + $action
---
+++ togglesql.ps1
@@ -0,0 +1,24 @@
+# PowerShell cmdlet to check a service's status
+# ` is the Escape character
+# "SQLBrowser", 
+$services = "MSSQL`$SQLEXPRESS", "MSSQL`$SQL2008", "SQLWriter"
+$servicePrior = get-Service $services[1]
+$services[0] + " is currently " + $servicePrior.status
+
+foreach ($serviceName in $services)
+{
+	if ($servicePrior.status -eq  "Running")
+	{
+		"Stopping " + $serviceName
+		stop-service $serviceName -Force
+		$action = "stopped"
+	}
+	else
+	{
+		"Starting " + $serviceName
+		start-service $serviceName
+		$action = "started"
+	}
+}
+
+"The server is now: " + $action
